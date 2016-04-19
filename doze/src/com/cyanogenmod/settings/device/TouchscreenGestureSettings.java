KonstaT/android_device_zte_kis3

doze: add proximity wake
Change-Id: Ib02f4611e7b8bc09d540ca5c49aab68ef27f188b

doze/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -31,10 +31,12 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
     private static final String KEY_AMBIENT_DISPLAY_ENABLE = "ambient_display_enable";
     private static final String KEY_GESTURE_POCKET = "gesture_pocket";
     private static final String KEY_GESTURE_HAND_WAVE = "gesture_hand_wave";
+    private static final String KEY_GESTURE_PROXIMITY_WAKE = "proximity_wake_enable";
 
     private SwitchPreference mAmbientDisplayPreference;
     private SwitchPreference mPocketPreference;
     private SwitchPreference mHandwavePreference;
+    private SwitchPreference mProximityWakePreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,6 +52,9 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
         mPocketPreference.setEnabled(dozeEnabled);
         mHandwavePreference = (SwitchPreference) findPreference(KEY_GESTURE_HAND_WAVE);
         mHandwavePreference.setEnabled(dozeEnabled);
+        mHandwavePreference.setOnPreferenceChangeListener(mProximityListener);
+        mProximityWakePreference = (SwitchPreference) findPreference(KEY_GESTURE_PROXIMITY_WAKE);
+        mProximityWakePreference.setOnPreferenceChangeListener(mProximityListener);
 
         final ActionBar bar = getActionBar();
         bar.setDisplayHomeAsUpEnabled(true);
@@ -96,6 +101,21 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
                 mHandwavePreference.setEnabled(enable);
             }
             return ret;
+        }
+    };
+
+    private Preference.OnPreferenceChangeListener mProximityListener =
+        new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            if ((boolean) newValue) {
+                if (preference.getKey().equals(KEY_GESTURE_HAND_WAVE)) {
+                    mProximityWakePreference.setChecked(false);
+                } else if (preference.getKey().equals(KEY_GESTURE_PROXIMITY_WAKE)) {
+                    mHandwavePreference.setChecked(false);
+                }
+            }
+            return true;
         }
     };
 }

Split-Screen/android_device_lge_d851

doze/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -32,10 +32,12 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
     private static final String KEY_AMBIENT_DISPLAY_ENABLE = "ambient_display_enable";
     private static final String KEY_HAND_WAVE = "gesture_hand_wave";
     private static final String KEY_GESTURE_POCKET = "gesture_pocket";
+    private static final String KEY_PROXIMITY_WAKE = "proximity_wake_enable";
 
     private SwitchPreference mAmbientDisplayPreference;
     private SwitchPreference mHandwavePreference;
     private SwitchPreference mPocketPreference;
+    private SwitchPreference mProximityWakePreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,9 +52,13 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
         mHandwavePreference =
             (SwitchPreference) findPreference(KEY_HAND_WAVE);
         mHandwavePreference.setEnabled(dozeEnabled);
+        mHandwavePreference.setOnPreferenceChangeListener(mProximityListener);
         mPocketPreference =
             (SwitchPreference) findPreference(KEY_GESTURE_POCKET);
         mPocketPreference.setEnabled(dozeEnabled);
+        mProximityWakePreference =
+            (SwitchPreference) findPreference(KEY_PROXIMITY_WAKE);
+        mProximityWakePreference.setOnPreferenceChangeListener(mProximityListener);
 
         final ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -98,6 +104,21 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
                 mPocketPreference.setEnabled(enable);
             }
             return ret;
+        }
+    };
+
+    private Preference.OnPreferenceChangeListener mProximityListener =
+        new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            if ((boolean) newValue) {
+                if (preference.getKey().equals(KEY_HAND_WAVE)) {
+                    mProximityWakePreference.setChecked(false);
+                } else if (preference.getKey().equals(KEY_PROXIMITY_WAKE)) {
+                    mHandwavePreference.setChecked(false);
+                }
+            }
+            return true;
         }
     };
 }